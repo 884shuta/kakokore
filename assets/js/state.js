4cd88dc05872b2e68ab1bf7e0556566a3a3d98ac
@884shuta <shu1587ta@icloud.com>
2025-10-19 13:13:43 +0900

    .gitignoreにassets/.DS_Storeを追加し、state.jsの画像URLを更新

--- assets/js/state.js
+++ assets/js/state.js
@@ -4,7 +4,7 @@
       id: "1",
       name: "火焔型土器",
       site: "（サンプル）新潟県周辺の縄文遺跡",
-      potColor: "#C0763A",
+      potColor: "#f80aa5ff",
       imageUrl:"assets/img/2.webp",
       description: "（サンプル）口縁部に炎のような装飾が見られる豪壮な土器。展示では造形のリズムと左右対称の美しさに注目してみましょう。",
       question: {
@@ -21,8 +21,8 @@
       id: "2",
       name: "深鉢形土器",
       site: "（サンプル）東北地方の縄文遺跡",
-      potColor: "#A6623A",
-      imageUrl:"https://lh3.googleusercontent.com/pw/AP1GczO8FX0I4XxWp6Y8MhnHsBZTUIUbDBXjdD0FUNt5NV6ynQVsPJ4WBdIqqxEq1UtkK-9bbOtERTnLm4dJpZhYmWRttSl9VTVWa07wSUCbHATk2M24XzRgQubxo1iOtLMB6V-chdXVqs_kMbXMCWnubHruMnkzR9xDZlZvROCv2zcnl3iI9Lloq-y5BJUdbxJDhJsJZeh2LUU9grUZroAJ16leUFtIbKnTFXh1S3xRiCz9MmIO_bQcuCOZGgNPcZPbXigKHWeJiErTs-lJxFgNB5G2zHiyLKXeF6IPCyNwl5zUS24vdc8lyqCHWeBC4rsYcBf0SRxH9kJgmS2qwid08kWnaotBWpvelyUzJTSkg1T4GHbYZa2JaEW8vys26RjzlDiGQrWEgpGOHjVIZPUY_AGWwaYal0HJwt7Awa7-xbiK5M8xVQk9lY5AFfoqLvbnjXd_CvKIGWDwskEFtATrFYinlXOJgsQTcj2X2GVfBlBk02OR5SDSAmd_0b2lKa5_wWu_py07Jf-nwRboTw0dCS9kVoEf3D8wOYZCthrAyxSIWWStweKw7B_rSx_A4QvOTypQryWy9WDzRXppGdHbJVC-yjYJb0BCpCmxKiIMPTCYrUMu1a4RJ0lNOfGCNKoZosP1XPRsxt9mHEwPbY4xgd9cLHLrHwQqwjmlpUX_GPEFdd_JM2nEnhiTPTdmg2dmOCsf2iWMyhaefdDz0nukxSTQ8jkqwkN9ObufFOFGj47td9SnPD9CzLZysNvMf1H05LgeEKthUdy1F9dW2eibd_aet-_RRyt-9YUknwJobaJfIGVLpe1Vix5fKVTzaUhAQ3WMoiauGXvNSg9Na_i-ow5eqNwvRhJz4cBSrPHwL5tJPl18puBm89Igsoe4OKoSvTOFTpoh1kS9hL8XmRCOzr1HHZ-tjpk6y_a6d2fqDw1A0o5XLNfCOw=w1584-h2034-s-no?authuser=1s",
+      potColor: "#d0ff00ff",
+      imageUrl:"assets/img/kaen.webp",
       description: "（サンプル）深い鉢形で、煮炊きに適したと考えられる土器。縄目文様のパターンや焼成の色ムラが見どころです。",
       question: {
         text: "深鉢形土器の“深い”形は主に何に向いていたと考えられる？",
@@ -38,7 +38,7 @@
       id: "3",
       name: "愛絆土器",
       site: "（サンプル）大分県内の大分市周辺",
-      potColor: "#C0763A",
+      potColor: "#2d00f5ff",
       imageUrl:"assets/img/3.webp",
       description: "（サンプル）ほかのものと比べて形が大きい。よく出土する場所はゲームセンター、カラオケ等。",
       question: {
